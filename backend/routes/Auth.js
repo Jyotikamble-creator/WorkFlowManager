53e4025b8a7d7260c4d29c5d9091ece45a329e55
Jyoti kamble <jyotikamble5235@gmail.com>
2025-06-10 12:38:23 +0530

    feat(work):auth

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -11,7 +11,20 @@ router.post('/login',async(req,res)=>{
     const user=await User.findOne({username})
     if(!user||await bcrypt.compare.findOne(password,user.password)){
         return res.status(401).json({message:'Unauthorized'})
+
+        const token=jwt.sign({_id:user._id},process.env.JWT_SECRET)
+        res.json({token})
     }
     
     })
 module.exports=router
+
+
+router.post('/register',async(req,res)=>{
+    const {username,password} = req.body;
+    const user=await User.findOne({username})
+    if(user){
+        return res.status(401).json({message:'Unauthorized'})
+    }
+    const hashPassword=await bcrypt.hash(password,10)
+})
