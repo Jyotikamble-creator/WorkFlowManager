e825e5e2e4ee12f56831945cd3ce9be2aec95841
Jyoti kamble <jyotikamble5235@gmail.com>
2025-07-19 12:50:58 +0530

    feat(controller):comment

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,31 +1,40 @@
 const Comment = require('../models/comment');
 const Task = require('../models/task');
 
+// Add a comment to a task
 exports.addComment = async (req, res) => {
   const { taskId } = req.params;
   const { text } = req.body;
+
   try {
     const comment = new Comment({
       taskId,
       text,
       createdBy: req.user.id,
     });
+
     await comment.save();
 
-    await Task.findByIdAndUpdate(taskId, { $push: { comments: comment._id } });
+    await Task.findByIdAndUpdate(taskId, {
+      $push: { comments: comment._id },
+    });
 
     res.status(201).json(comment);
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    console.error('Error adding comment:', err);
+    res.status(500).json({ message: 'Server error while adding comment' });
   }
 };
 
+// Get all comments for a specific task
 exports.getComments = async (req, res) => {
   const { taskId } = req.params;
+
   try {
-    const comments = await Comment.find({ taskId }).populate('createdBy', 'name');
-    res.json(comments);
+    const comments = await Comment.find({ taskId }).populate('createdBy', 'name email');
+    res.status(200).json(comments);
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    console.error('Error fetching comments:', err);
+    res.status(500).json({ message: 'Server error while fetching comments' });
   }
 };
